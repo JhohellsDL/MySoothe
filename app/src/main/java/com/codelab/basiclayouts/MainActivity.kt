5daa3745989ea78776523c30f86ed6cb33ec05ff
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2022-10-24 23:17:58 -0500

    implementation app final

--- app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -240,13 +240,13 @@ private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
         )
         BottomNavigationItem(
             icon = {
-                   Icon(
-                       imageVector = Icons.Default.AccountCircle,
-                       contentDescription = null
-                   )
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
             },
             label = {
-                  Text(text = stringResource(id = R.string.bottom_navigation_profile))
+                Text(text = stringResource(id = R.string.bottom_navigation_profile))
             },
             selected = true,
             onClick = {})
@@ -256,7 +256,14 @@ private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
 // Step: MySoothe App - Scaffold
 @Composable
 fun MySootheApp() {
-    // Implement composable here
+    MySootheTheme {
+        Scaffold(
+            bottomBar = {
+                SootheBottomNavigation()
+            }) { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
 }
 
 private val alignYourBodyData = listOf(
